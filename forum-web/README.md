32fb53628ce9f8940b61b6c42c6e0347a31ba815
Jerry Wang <jerryw4113@gmail.com>
2018-08-30 21:46:31 +1000

    Added initial phpbb files, not set up - sorting out database

---
+++ forum-web/README.md
@@ -0,0 +1,5 @@
+Install php-cli
+sudo apt-get install php php-xml
+
+Start PHP server
+php -S localhost:<PORT>
---
+++ forum-web/store/install_config.php
@@ -0,0 +1,1 @@
+<?php // {"installer_config":[],"progress_data":{"last_task_module_name":"","last_task_module_index":0,"last_task_index":0,"max_task_progress":13,"current_task_progress":0,"_restart_points":[],"use_restart_point":false},"navigation_data":{"finished":[["install",0,"introduction"]],"active":["install",0,"requirements"]}}
