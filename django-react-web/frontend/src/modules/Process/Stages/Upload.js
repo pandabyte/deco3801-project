c46962d7cb072afaf5b7d753f91ae67b3a52bcf6
Minh Nguyen <s4355206@student.uq.edu.au>
2018-08-31 03:17:26 +1000

    added file uploads for single and multiple

--- django-react-web/frontend/src/modules/Process/Stages/Upload.js
+++ django-react-web/frontend/src/modules/Process/Stages/Upload.js
@@ -1,22 +1,48 @@
 import * as React from 'react';
-import { Icon, Divider, Header, Button } from 'semantic-ui-react';
+import { Header, Label, Button, Icon, Input, Form } from 'semantic-ui-react';
 import ProcessStore from '../ProcessStore';
 import { observer } from 'mobx-react';
 
 @observer
 export default class Upload extends React.Component {
 
-    componentDidMount = () => {
+    onUploadSingleFile = (e) => {
+        console.log("uploading single file");
 
     }
+
+    onUploadMultipleFile = (e) => {
+        console.log("uploading multiple file");
+    }
+
 
     render() {
 
 
         return (
             <div>
-                
+
                 <Header> Uploading Stage </Header>
+
+                <div >
+
+                    <div className='d-inline-block'>
+                        <Label as="label" htmlFor="singleFile" size="big">
+                            <Icon name='file text' size='big' />
+                            Upload Single File
+                    </Label>
+                        <input id="singleFile" hidden type="file" />
+                    </div>
+
+                    <div className='d-inline-block'>
+                        <Label as="label" htmlFor="multipleFile" size="big">
+                            <Icon name='file text' size='big' />
+                            Upload Multiple File
+                    </Label>
+                        <input id="multipleFile" hidden type="file" multiple />
+                    </div>
+                </div>
+
 
             </div>
         )
