04f69a2b3f757bbc2b4b0e65f8b2a6dbcce5b0e1
Andre Wong <andre.wong@uqconnect.edu.au>
2018-09-03 17:59:58 +1000

    Import bootstrap

--- django-react-web/frontend/src/index.js
+++ django-react-web/frontend/src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 // import style sheets
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import custom components
 import App from './App';
