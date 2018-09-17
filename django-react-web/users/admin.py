812d0ad3df7427577f386b74292a1d2c7d8af693
Andre Wong <andre.wong@uqconnect.edu.au>
2018-09-17 23:30:18 +1000

    Added custom user admin forms for custom user

--- django-react-web/users/admin.py
+++ django-react-web/users/admin.py
@@ -1,6 +1,37 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, UsernameField
+from django.utils.translation import gettext_lazy as _
+
 from .models import User, Affiliation
 
-admin.site.register(User, UserAdmin)
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('email', 'username', 'first_name', 'last_name')
+        field_classes = {'email': UsernameField, 'username': UsernameField}
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'username')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (None, {'fields': ('aid', 'position')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Affiliation)
