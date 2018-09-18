4a9e4da48aa3fada0eb10c168706af591497fc2d
Jerry Wang <chunyin.wang@uqconnect.edu.au>
2018-09-18 19:21:14 +1000

    Added endpoint to return user profile

--- django-react-web/api/v1/urls.py
+++ django-react-web/api/v1/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from . import views
+import users.views as userViews
 
 urlpatterns = [
     path('register/', views.register),
     path('userid/', views.user_id),
+    path('userprofile/', userViews.user_profile)
 ]
