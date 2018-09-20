981c9bb9b2c955046317feb7478827715f0e4c5e
Minh Nguyen <s4355206@student.uq.edu.au>
2018-09-20 23:44:21 +1000

    adjust footer

--- django-react-web/frontend/src/modules/Layout/AppFooter.js
+++ django-react-web/frontend/src/modules/Layout/AppFooter.js
@@ -5,44 +5,46 @@ export default class AppFooter extends React.Component {
 
     render() {
         return (
+            <div className='bg-dark text-center'>
 
-            <Container className='bg-dark text-center'>
-                
-                {/*  Main footer content */}
-                <Grid divided='vertically'>
-                    <Grid.Row columns={3} className='text-white text-left p-5'>
+                <Container >
+                    {/*  Main footer content */}
+                    <Grid divided='vertically'>
+                        <Grid.Row columns={3} className='text-white text-left p-5'>
 
-                        <Grid.Column>
-                            <Header className='text-white' as='h2'> QAHES</Header>
-                            <p>123 Abc Street, 4075, Brisbane</p>
-                            <p>(+61) 400 000 000</p>
-                        </Grid.Column>
+                            <Grid.Column>
+                                <Header className='text-white' as='h2'> QAHES</Header>
+                                <p>123 Abc Street, 4075, Brisbane</p>
+                                <p>(+61) 400 000 000</p>
+                            </Grid.Column>
 
-                        <Grid.Column >
-                            <Header className='text-white' as='h2'> _ANOTHER COLUMN_</Header>
-                            <p>Content One</p>
-                            <p>Content Two</p>
-                        </Grid.Column>
+                            <Grid.Column >
+                                <Header className='text-white' as='h2'> _ANOTHER COLUMN_</Header>
+                                <p>Content One</p>
+                                <p>Content Two</p>
+                            </Grid.Column>
 
-                        <Grid.Column >
-                            <Header className='text-white' as='h2'> COMPANY</Header>
-                            <p>About Us</p>
-                            <p>Contact Us</p>
-                        </Grid.Column>
+                            <Grid.Column >
+                                <Header className='text-white' as='h2'> COMPANY</Header>
+                                <p>About Us</p>
+                                <p>Contact Us</p>
+                            </Grid.Column>
 
-                    </Grid.Row>
-                </Grid>
+                        </Grid.Row>
+                    </Grid>
 
-                <Divider className='bg-white' />
+                    <Divider className='bg-white' />
 
-                {/*  T&A footer content */}
-                <div className='text-center text-white p-5'>
-                    <span className='p-3'>Privacy</span>
-                    <span className='p-3'>Terms</span>
-                    <span className='p-3'>Sitemap</span>
-                </div>
+                    {/*  T&A footer content */}
+                    <div className='text-center text-white p-5'>
+                        <span className='p-3'>Privacy</span>
+                        <span className='p-3'>Terms</span>
+                        <span className='p-3'>Sitemap</span>
+                    </div>
 
-            </Container>
+                </Container>
+            </div>
+
         )
     }
 }
