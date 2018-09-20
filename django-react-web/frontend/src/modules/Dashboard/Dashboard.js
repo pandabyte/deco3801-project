83036ed2c4643aaed1ab18cf6f56a41285cfe1d8
Minh Nguyen <s4355206@student.uq.edu.au>
2018-09-20 23:41:16 +1000

    mapping dashboard tabs

--- django-react-web/frontend/src/modules/Dashboard/Dashboard.js
+++ django-react-web/frontend/src/modules/Dashboard/Dashboard.js
@@ -1,17 +1,62 @@
 import * as React from 'react';
 import { Container, Icon, Grid } from 'semantic-ui-react';
+import { observer } from 'mobx-react';
 
+@observer
 export default class Dashboard extends React.Component {
+
+    redirectToTarget = (e) => {
+        this.props.history.push('/' + e.target.id)
+    }
 
     render() {
 
         /* Array of tabs. One tab is associated with one react page */
         const tabs = [
             {
+                iconName: 'user',
+                tabName: 'Profile',
+                url: '/profile'
+            },
+            {
+                iconName: 'users',
+                tabName: 'Users',
+                url: '/users'
+            },
+            {
                 iconName: 'cloud upload',
                 tabName: 'Upload and process my data',
-                url: '/uploadprocess',
-                onClick: 'handler'
+                url: '/process'
+            },
+            {
+                iconName: 'cloud download',
+                tabName: 'View or download saved reports',
+                url: '/reports'
+            },
+            {
+                iconName: 'globe',
+                tabName: 'Perform a global search for a chemical compound',
+                url: '/search'
+            },
+            {
+                iconName: 'wechat',
+                tabName: 'Forum and CEC alerts',
+                url: '/forum'
+            },
+            {
+                iconName: 'wrench',
+                tabName: 'Hardware and acquisition methods',
+                url: '/acquisition'
+            },
+            {
+                iconName: 'graduation cap',
+                tabName: 'Documentation and Education',
+                url: '/information'
+            },
+            {
+                iconName: 'help',
+                tabName: 'Help and FAQ',
+                url: '/help'
             }
         ];
 
@@ -21,56 +66,11 @@ export default class Dashboard extends React.Component {
                 {/* Map each tab to its presentation*/}
                 <Grid columns={3} padded>
                     {tabs.map((tab, index) => {
-                        return (<Grid.Column>
+                        return (<Grid.Column key={index}>
                             <Icon name={tab.iconName} size='massive' />
-                            <p>{tab.tabName}</p>
+                            <p id={tab.url} onClick={this.redirectToTarget}>{tab.tabName}</p>
                         </Grid.Column>)
                     })}
-                </Grid>
-
-
-                <Grid columns={3} padded>
-                    <Grid.Column>
-                        <Icon name='cloud upload' size='massive' />
-                        <p>Upload and process my data</p>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Icon name='cloud download' size='massive' />
-                        <p>View or download my saved reports</p>
-                    </Grid.Column>
-
-                    <Grid.Column>
-                        <Icon name='globe' size='massive' />
-                        <p>Perform a global serach for a compound</p>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Icon name='user' size='massive' />
-                        <p>My profile</p>
-                    </Grid.Column>
-
-                    <Grid.Column>
-                        <Icon name='wrench' size='massive' />
-                        <p>My hardward and acquisition method</p>
-                    </Grid.Column>
-
-                    <Grid.Column>
-                        <Icon name='wechat' size='massive' />
-                        <p>Forum and cec alerts</p>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Icon name='graduation cap' size='massive' />
-                        <p>Documentation and education</p>
-                    </Grid.Column>
-
-                    <Grid.Column>
-                        <Icon name='users' size='massive' />
-                        <p>Reseracher profiles</p>
-                    </Grid.Column>
-
-                    <Grid.Column>
-                        <Icon name='help' size='massive' />
-                        <p>Help</p>
-                    </Grid.Column>
                 </Grid>
             </Container>
         )
