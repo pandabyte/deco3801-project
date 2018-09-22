6219af3113301c5338f31cdd78b79a6d162da4ce
Jerry Wang <chunyin.wang@uqconnect.edu.au>
2018-09-23 02:01:47 +1000

    Changed user_profile api to get

--- django-react-web/api/v1/urls.py
+++ django-react-web/api/v1/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from . import views
-import users.views as userViews
+import users.views as user_views
 
 urlpatterns = [
     path('register/', views.register),
     path('users/', views.users),
     path('userid/', views.user_id),
-    path('userprofile/', userViews.user_profile)
+    path('userprofile/', user_views.user_profile),
+    path('userprofile/update', user_views.user_profile_update)
 ]
