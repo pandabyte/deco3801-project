104ccf4f9f54b2dae1419a923ed1ff92afcf5bf9
Jerry Wang <chunyin.wang@uqconnect.edu.au>
2018-09-25 18:10:36 +1000

    Fixed naming convention for deprecated decorator

--- django-react-web/api/utils.py
+++ django-react-web/api/utils.py
@@ -3,11 +3,11 @@ from rest_framework.response import Response
 
 
 def deprecated(func):
-    def deprecatedFunc(self):
+    def deprecatedFunc(func):
             return Response('Error: This API is deprecated, please check the documentation for more detail', status=status.HTTP_410_GONE)
     return deprecatedFunc
 
 def deprecated_warn(func):
-    def deprecatedFunc(self):
+    def deprecatedFunc(func):
             return Response('Warning: This API will soon be deprecated, please check the documentation for more detail', status=299)
     return deprecatedFunc
