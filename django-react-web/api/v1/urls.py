6cec69b9a34da3731b87267902a1af28359dbefb
Jerry Wang <chunyin.wang@uqconnect.edu.au>
2018-09-30 03:28:25 +1000

    Set up view function to generate one time token for password recovery
    - Setting up SMTP server in progress
    - Tokens are manually created using pyjwt as simple-jwt does not support manually generating tokens easily

--- django-react-web/api/v1/urls.py
+++ django-react-web/api/v1/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path('userid/', views.user_id),
     path('userprofile/', user_views.user_profile),
     path('userprofile/update', user_views.user_profile_update),
+    path('password-recovery/request/', views.password_recovery_request),
+    path('password-recovery/landing/', views.password_recovery_landing),
+    path('password-recovery/submit/', views.password_recovery_submit),
 
     path('test/', views.deprecate_test)
 ]
