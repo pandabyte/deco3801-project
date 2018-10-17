0d59b909e76bfbcd3338a02c98318c987f44c6f0
Andre Wong <andre.wong@uqconnect.edu.au>
2018-10-17 15:20:44 +1000

    Added file upload owner

--- django-react-web/home/views.py
+++ django-react-web/home/views.py
@@ -1,10 +1,12 @@
+from django.conf import settings
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
+import jwt
 
 from .models import FileUpload
 from .forms import UploadForm
+from users.models import User
 
 def index(request):
     return render(request, 'home/index.html')
@@ -13,7 +15,12 @@ def upload(request):
     if request.method == 'POST':
         form = UploadForm(request.POST, request.FILES)
         if form.is_valid():
-            instance = FileUpload(file=request.FILES['file'])
+            try:
+                token = jwt.decode(request.POST['token'], settings.SECRET_KEY, algorithm='HS256')
+                user = User.objects.get(id=token['user_id'])
+            except:
+                return HttpResponse(status=422)
+            instance = FileUpload(file=request.FILES['file'], owner=user)
             instance.save()
             return HttpResponse(status=201)
     else:
