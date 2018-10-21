afcc220b58564a3e959df05241745f64bd6110d6
Andre Wong <andre.wong@uqconnect.edu.au>
2018-10-22 00:04:33 +1000

    Added API tests

--- django-react-web/api/v1/tests.py
+++ django-react-web/api/v1/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestApiV1(TestCase):
+    def test_register_correct(self):
+        """
+        Test registering a new user.
+        """
+        response = self.client.post(
+            '/api/v1/register/',
+            {
+                'email': 'user@example.com',
+                'first_name': 'first',
+                'last_name': 'last',
+                'password': 'password',
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_missing(self):
+        """
+        Test registering a new user without an email field.
+        """
+        response = self.client.post(
+            '/api/v1/register/',
+            {
+                'first_name': 'first',
+                'last_name': 'last',
+                'password': 'password',
+            },
+        )
+        self.assertEqual(response.status_code, 422)
