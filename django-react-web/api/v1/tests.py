96d3d41e85da8aecc77a72e76fcbb51de6bc7332
Andre Wong <andre.wong@uqconnect.edu.au>
2018-10-22 00:43:23 +1000

    Added test for users

--- django-react-web/api/v1/tests.py
+++ django-react-web/api/v1/tests.py
@@ -1,14 +1,35 @@
+import json
 from django.test import TestCase
 
+from users.models import User
+from users.serializers import UserSerializer
+
 class TestApiV1(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email='user@example.com',
+            first_name='first',
+            last_name='last',
+            password='password',
+        )
+        response = self.client.post(
+            '/api/token/obtain/',
+            {
+                'email': 'user@example.com',
+                'password': 'password',
+            }
+        )
+        self.token = json.loads(response.content.decode('utf-8'))['access']
+
     def test_register_correct(self):
         """
         Test registering a new user.
         """
+        # Create new user
         response = self.client.post(
             '/api/v1/register/',
             {
-                'email': 'user@example.com',
+                'email': 'newuser@example.com',
                 'first_name': 'first',
                 'last_name': 'last',
                 'password': 'password',
@@ -16,10 +37,19 @@ class TestApiV1(TestCase):
         )
         self.assertEqual(response.status_code, 200)
 
-    def test_register_missing(self):
-        """
-        Test registering a new user without an email field.
-        """
+        # Fail to register user with same email
+        response = self.client.post(
+            '/api/v1/register/',
+            {
+                'email': 'newuser@example.com',
+                'first_name': 'first',
+                'last_name': 'last',
+                'password': 'password',
+            },
+        )
+        self.assertEqual(response.status_code, 422)
+
+        # Fail to register user with no email
         response = self.client.post(
             '/api/v1/register/',
             {
@@ -29,3 +59,24 @@ class TestApiV1(TestCase):
             },
         )
         self.assertEqual(response.status_code, 422)
+
+    def test_user_list(self):
+        """
+        Test getting the list of users.
+        """
+        # Get users without authorization
+        response = self.client.get(
+            '/api/v1/users/'
+        )
+        self.assertEqual(response.status_code, 401)
+
+        # Get users with authorization
+        response = self.client.get(
+            '/api/v1/users/',
+            HTTP_AUTHORIZATION='Bearer ' + self.token,
+        )
+        all_users = User.objects.all()
+        serializer = UserSerializer(all_users, many=True)
+        expected = [dict(user) for user in serializer.data]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, json.dumps(expected).encode('utf-8'))
