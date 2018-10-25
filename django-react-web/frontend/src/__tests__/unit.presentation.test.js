eb0f6e19f59ef19abf926043f7f96daa4e44d37e
Minh Nguyen <s4355206@student.uq.edu.au>
2018-10-25 18:26:08 +1000

    fix tests

--- django-react-web/frontend/src/__tests__/unit.presentation.test.js
+++ django-react-web/frontend/src/__tests__/unit.presentation.test.js
@@ -43,20 +43,20 @@ describe('Set: Test shallow rendering of React component', () => {
     test("Shallow Test: Search Component", () => {
         const wrapper = shallow(<ChemicalSearch />);
         expect(wrapper.find('div').length).toEqual(1);
-        expect(wrapper.find(Header).length).toEqual(2);
+        expect(wrapper.find(Header).length).toEqual(1);
         expect(wrapper.find(Search).length).toEqual(1);
-        expect(wrapper.find(Divider).length).toEqual(2);
-        expect(wrapper.find(Segment).length).toEqual(2);
+        expect(wrapper.find(Divider).length).toEqual(1);
+        expect(wrapper.find(Segment).length).toEqual(1);
     });
 
     test("Shallow Test: Dashboard Component", () => {
         const wrapper = shallow(<Dashboard />);
         expect(wrapper.find(Container).length).toEqual(1);
         expect(wrapper.find(Grid).length).toEqual(1);
-        expect(wrapper.find(Grid.Column).length).toEqual(9);
-        expect(wrapper.find(Icon).length).toEqual(9);
-        expect(wrapper.find(Header).length).toEqual(9);
-        expect(wrapper.find(Menu.Item).length).toEqual(9);
+        expect(wrapper.find(Grid.Column).length).toEqual(8);
+        expect(wrapper.find(Icon).length).toEqual(8);
+        expect(wrapper.find(Header).length).toEqual(8);
+        expect(wrapper.find(Menu.Item).length).toEqual(8);
     });
 });
 
